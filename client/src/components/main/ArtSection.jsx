33346cdfc0c360eeffb7c24c95335e14f1ba2350
Pix3lAssassin <th3koolperson@gmail.com>
2021-06-22 00:02:35 -0600

    added more dependencies and preped for threejs

--- client/src/components/main/ArtSection.jsx
+++ client/src/components/main/ArtSection.jsx
@@ -1,15 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import * as Three from 'three';
 
 import {
   Container
 } from '@material-ui/core';
 
 export default function ArtSection({ imageData }) {
+  const mount = useRef(null);
+
+  useEffect(() => {
+    let width = mount.current.clientWidth;
+    let height = mount.current.clientHeight;
+    let frameId;
+
+    const scene = new Three.Scene();
+
+    const camera = new Three.PerspectiveCamera(75, 16 / 9, 0.1, 1000);
+    const renderer = new Three.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+    renderer.setClearColor('#000000');
+
+    camera.position.setZ(30);
+
+    const renderScene = () => {
+      renderer.render(scene, camera);
+    };
+
+    const handleResize = () => {
+      width = mount.current.clientWidth;
+      height = mount.current.clientHeight;
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderScene();
+    };
+
+    const animate = () => {
+      renderScene();
+      frameId = window.requestAnimationFrame(animate);
+    };
+
+    mount.current.appendChild(renderer.domElement);
+    window.addEventListener('resize', handleResize);
+    frameId = requestAnimationFrame(animate);
+  }, []);
+
   return (
     <Container className="questionContainer" style={{ padding: 0 }}>
 
-      <canvas />
+      <div ref={mount} />
 
     </Container>
   );
--- server/routes/images.js
+++ server/routes/images.js
@@ -4,64 +4,8 @@ const axios = require('axios');
 const router = express.Router();
 
 // Get request for a question with a default page of 1
-router.get('/questions', (req, res) => {
-  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions', {
-    headers: {
-      Authorization: GITHUB_API_KEY
-    },
-    params: {
-      product_id: req.query.product_id,
-      page: req.query.page || 1
-    }
-  })
-    .then((response) => response.data)
-    .then((questions) => {
-      res.send(questions);
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-console
-      console.log(err);
-      res.send(err);
-    });
-});
-
-// Post request for a new question
-router.post('/questions', (req, res) => {
-  axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions', {
-    body: req.body.body,
-    name: req.body.name,
-    email: req.body.email,
-    product_id: req.body.product_id
-  }, {
-    headers: {
-      Authorization: GITHUB_API_KEY
-    }
-  })
-    .then((response) => {
-      res.status(201).send(response.data);
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-console
-      console.log(err);
-      res.send(err);
-    });
-});
-
-// Update the helpfulness of a question
-router.put('/questions/helpful', (req, res) => {
-  axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions/${req.query.question_id}/helpful`, {}, {
-    headers: {
-      Authorization: GITHUB_API_KEY
-    }
-  })
-    .then(() => {
-      res.status(204).send();
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-console
-      console.log(err);
-      res.send(err);
-    });
+router.get('/', (req, res) => {
+  res.status(200).send([]);
 });
 
 module.exports.router = router;
