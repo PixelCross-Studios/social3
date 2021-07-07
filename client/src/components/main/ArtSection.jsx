f2bce0b18eaf26eae3fa0e4393fe1fd259ae506b
Pix3lAssassin <th3koolperson@gmail.com>
2021-07-07 12:27:20 -0600

    renders cube to screen

--- client/src/components/main/ArtSection.jsx
+++ client/src/components/main/ArtSection.jsx
@@ -1,19 +1,16 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
+import { Container } from '@material-ui/core';
 import * as Three from 'three';
 
-import {
-  Container
-} from '@material-ui/core';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export default function ArtSection({ imageData }) {
   const mount = useRef(null);
 
   useEffect(() => {
     let width = mount.current.clientWidth;
-    let height = mount.current.clientHeight;
-    let frameId;
+    let height = width * 0.5625;
 
     const scene = new Three.Scene();
 
@@ -23,7 +20,40 @@ export default function ArtSection({ imageData }) {
     renderer.setSize(width, height);
     renderer.setClearColor('#000000');
 
-    camera.position.setZ(30);
+    camera.position.setZ(1.5);
+
+    const addVoxel = (position = new Three.Vector3(0, 0, 0), color = 0xFF00FF) => {
+      const geometry = new Three.BoxGeometry(1, 1, 1);
+      const material = new Three.MeshStandardMaterial({ color });
+      material.roughness = 0.5;
+      const voxel = new Three.Mesh(geometry, material);
+
+      voxel.position.set(position.x, position.y, position.z);
+      scene.add(voxel);
+    };
+
+    addVoxel();
+
+    const directionalLight = new Three.DirectionalLight(0xFFFFFF, 1);
+    directionalLight.position.set(5, 5, 5);
+
+    const ambientLight = new Three.AmbientLight(0x808080);
+    scene.add(directionalLight, ambientLight);
+
+    const lightHelper = new Three.DirectionalLightHelper(directionalLight);
+    const gridHelper = new Three.GridHelper(128, 64);
+    scene.add(lightHelper, gridHelper);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.1;
+    controls.minPolarAngle = Math.PI / 2 - Math.PI * 0.2;
+    controls.maxPolarAngle = Math.PI / 2 + Math.PI * 0.2;
+    controls.minAzimuthAngle = -1;
+    controls.maxAzimuthAngle = 1;
+    controls.rotateSpeed = 0.2;
 
     const renderScene = () => {
       renderer.render(scene, camera);
@@ -31,7 +61,7 @@ export default function ArtSection({ imageData }) {
 
     const handleResize = () => {
       width = mount.current.clientWidth;
-      height = mount.current.clientHeight;
+      height = width * 0.5625;
       renderer.setSize(width, height);
       camera.aspect = width / height;
       camera.updateProjectionMatrix();
@@ -39,13 +69,24 @@ export default function ArtSection({ imageData }) {
     };
 
     const animate = () => {
+      controls.update();
+
       renderScene();
-      frameId = window.requestAnimationFrame(animate);
+      requestAnimationFrame(animate);
     };
 
     mount.current.appendChild(renderer.domElement);
     window.addEventListener('resize', handleResize);
-    frameId = requestAnimationFrame(animate);
+    animate();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      mount.current.removeChild(renderer.domElement);
+
+      scene.remove(box);
+      geometry.dispose();
+      material.dispose();
+    };
   }, []);
 
   return (
